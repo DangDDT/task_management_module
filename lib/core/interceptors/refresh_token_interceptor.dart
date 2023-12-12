18ad8afe42fb080cf70ebabfe534c6e2ad88ef4e
Do Duong Tam Dang <dangddt@outlook.com>
2023-12-13 00:41:27 +0700

    Update refresh_token_interceptor.dart

--- lib/core/interceptors/refresh_token_interceptor.dart
+++ lib/core/interceptors/refresh_token_interceptor.dart
@@ -33,23 +33,27 @@ class RefreshTokenInterceptor implements Interceptor {
     if (error.response != null) {
       if (error.response!.statusCode == 401) {
         try {
-          // final requestOptions = error.requestOptions;
-          // final newAccessToken =
-          //     await _config.getAuthConfig?.onRefreshTokenCallback?.call();
-          // if (newAccessToken == null) return handler.next(error);
-          // final opts = Options(method: requestOptions.method);
-          // final dioClient = Dio();
-          // dioClient.options.headers['Authorization'] = 'Bearer $newAccessToken';
-          // dioClient.options.headers['Accept'] = '*/*';
-          // final response = await dioClient.request<dynamic>(
-          //   requestOptions.path,
-          //   options: opts,
-          //   cancelToken: requestOptions.cancelToken,
-          //   onReceiveProgress: requestOptions.onReceiveProgress,
-          //   data: requestOptions.data,
-          //   queryParameters: requestOptions.queryParameters,
-          // );
-          return _config.getAuthConfig?.onUnauthorizedCallback?.call();
+          final requestOptions = error.requestOptions;
+          final newAccessToken =
+              await _config.getAuthConfig?.onRefreshTokenCallback?.call();
+          if (newAccessToken == null) return handler.next(error);
+          final opts = Options(method: requestOptions.method);
+          final dioClient = Dio();
+          dioClient.options.headers['Authorization'] = 'Bearer $newAccessToken';
+          dioClient.options.headers['Accept'] = '*/*';
+          final response = await dioClient.request<dynamic>(
+            requestOptions.path,
+            options: opts,
+            cancelToken: requestOptions.cancelToken,
+            onReceiveProgress: requestOptions.onReceiveProgress,
+            data: requestOptions.data,
+            queryParameters: requestOptions.queryParameters,
+          );
+          if (response.statusCode == 200) {
+            return handler.resolve(response);
+          } else {
+            return handler.next(error);
+          }
         } catch (e) {
           return handler.next(error);
         }
